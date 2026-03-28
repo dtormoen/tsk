dtormoen/tsk-tsk

feat: add `tsk review` command with auto-detect, task, and base modes
Implement the core `tsk review` command with three modes:

- Auto-detect (no args): extracts task ID from current tsk branch name,
  or falls back to git-town parent branch as base ref
- Task review (task_id): reviews a completed task's changes, resolving
  the base commit via source_commit ancestry, git-town, or source_branch
  merge-base. Chains reviews with proper numbering and parent linking.
- Base review (--base): reviews changes against an arbitrary git ref

The command opens an editor (via review_command config or $EDITOR) to
collect review feedback, then queues a tsk-review task that chains to
the reviewed task with branch reuse via target_branch.

Supporting changes:
- Add git helpers: get_git_town_parent, is_ancestor, merge_base, rev_parse
- Remove #[cfg(test)] gate from TaskBuilder::skip_parent_repo_deferral
- Add TaskBuilder::source_commit_override for custom base commits
- Update README.md review command documentation

src/task_builder.rs
@@ -47,6 +47,9 @@ pub struct TaskBuilder {
     /// instead of deferring to the scheduler. Used for review tasks where the
     /// parent is already complete.
     skip_parent_repo_deferral: bool,
+    /// Override for the source commit SHA. When set, this value is used instead
+    /// of computing it from the repository HEAD.
+    source_commit_override: Option<String>,
 }
 
 impl TaskBuilder {
@@ -71,6 +74,7 @@ impl TaskBuilder {
             repo_copy_source: None,
             target_branch: None,
             skip_parent_repo_deferral: false,
+            source_commit_override: None,
         }
     }
 
@@ -201,10 +205,15 @@ impl TaskBuilder {
 
     /// When true, tasks with a parent_id copy the repo at creation time instead
     /// of deferring to the scheduler. The parent's resolved config is inherited.
-    /// Gated to `cfg(test)` until the review command is implemented.
-    #[cfg(test)]
     pub fn skip_parent_repo_deferral(mut self, skip: bool) -> Self {
         self.skip_parent_repo_deferral = skip;
+        self
+    }
+
+    /// Sets a custom source commit SHA instead of using the repository HEAD.
+    /// Used by review tasks to set the base commit for the diff.
+    pub fn source_commit_override(mut self, sha: Option<String>) -> Self {
+        self.source_commit_override = sha;
         self
     }
 
@@ -361,11 +370,17 @@ impl TaskBuilder {
                 .await?
         };
 
-        // Capture the current commit SHA
-        let source_commit = match git_operations::get_current_commit(&repo_root).await {
-            Ok(commit) => commit,
-            Err(e) => {
-                return Err(format!("Failed to get current commit for task '{name}': {e}").into());
+        // Capture the current commit SHA (or use override if provided)
+        let source_commit = if let Some(ref override_sha) = self.source_commit_override {
+            override_sha.clone()
+        } else {
+            match git_operations::get_current_commit(&repo_root).await {
+                Ok(commit) => commit,
+                Err(e) => {
+                    return Err(
+                        format!("Failed to get current commit for task '{name}': {e}").into(),
+                    );
+                }
             }
         };
 

dtormoen/tsk

src/task_builder.rs
@@ -47,6 +47,9 @@ pub struct TaskBuilder {
     /// instead of deferring to the scheduler. Used for review tasks where the
     /// parent is already complete.
     skip_parent_repo_deferral: bool,
+    /// Override for the source commit SHA. When set, this value is used instead
+    /// of computing it from the repository HEAD.
+    source_commit_override: Option<String>,
 }
 
 impl TaskBuilder {
@@ -71,6 +74,7 @@ impl TaskBuilder {
             repo_copy_source: None,
             target_branch: None,
             skip_parent_repo_deferral: false,
+            source_commit_override: None,
         }
     }
 
@@ -201,10 +205,15 @@ impl TaskBuilder {
 
     /// When true, tasks with a parent_id copy the repo at creation time instead
     /// of deferring to the scheduler. The parent's resolved config is inherited.
-    /// Gated to `cfg(test)` until the review command is implemented.
-    #[cfg(test)]
     pub fn skip_parent_repo_deferral(mut self, skip: bool) -> Self {
         self.skip_parent_repo_deferral = skip;
+        self
+    }
+
+    /// Sets a custom source commit SHA instead of using the repository HEAD.
+    /// Used by review tasks to set the base commit for the diff.
+    pub fn source_commit_override(mut self, sha: Option<String>) -> Self {
+        self.source_commit_override = sha;
         self
     }
 
@@ -364,11 +373,17 @@ impl TaskBuilder {
                 .await?
         };
 
-        // Capture the current commit SHA
-        let source_commit = match git_operations::get_current_commit(&source_info_path).await {
-            Ok(commit) => commit,
-            Err(e) => {
-                return Err(format!("Failed to get current commit for task '{name}': {e}").into());
+        // Capture the current commit SHA (or use override if provided)
+        let source_commit = if let Some(ref override_sha) = self.source_commit_override {
+            override_sha.clone()
+        } else {
+            match git_operations::get_current_commit(&source_info_path).await {
+                Ok(commit) => commit,
+                Err(e) => {
+                    return Err(
+                        format!("Failed to get current commit for task '{name}': {e}").into(),
+                    );
+                }
             }
         };
 